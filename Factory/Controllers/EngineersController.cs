b227b51f2d3ab4d2e998f5eba15f315951f6042e
Austin Schrader <austin.jschrader@gmail.com>
2021-01-10 20:30:56 -0800

    Add details page for engineers

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -39,6 +39,15 @@ namespace Factory.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+          .Include(engineer => engineer.Machines)
+          .ThenInclude(join => join.Machine)
+          .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
 
   }
 }
