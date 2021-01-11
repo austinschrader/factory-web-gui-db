d5b3ee09f76907fbcaefdd113686b5c65e72856f
Austin Schrader <austin.jschrader@gmail.com>
2021-01-10 21:51:06 -0800

    Add page to delete machines

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -58,6 +58,12 @@ namespace Factory.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Delete(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
 
   }
 }
